05f84f198b83a03e46619c16fc384c94cfcc9a6f
felix <mail@felixriehm.de>
2021-05-12 09:36:54 +0200

    refine code

--- Assets/Scripts/GameBall.cs
+++ Assets/Scripts/GameBall.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class GameBall : MonoBehaviour
 {
@@ -12,69 +13,72 @@ public class GameBall : MonoBehaviour
     public UnityEvent OnEnemyGoalHit { get; set; }
     public UnityEvent OnThrowableObstacleHit { get; set; }
     private Vector3 _targetLocal;
-    private Vector3 direction;
-    private bool move = false;
+    private Vector3 _lastTargetLocal;
+    private Vector3 _direction;
+    private bool _move = false;
     private float _step;
     private int _bounceLayerMask;
 
     private void Start()
     {
         _bounceLayerMask = LayerMask.GetMask("Bounce");
+        _lastTargetLocal = transform.parent.InverseTransformPoint(transform.position);
     }
 
     public void initForce()
     {
-        float xDirection = 1;//Random.Range(0.0f, 1.0f);
-        float zDirection = 0.7f;//Random.Range(-1.0f, 1.0f);
-        direction = new Vector3(xDirection, 0, zDirection);
+        float xDirection = Random.Range(-1.0f, 1.0f);
+        float zDirection = Random.Range(0.4f, 1f);
+        _direction = new Vector3(xDirection, 0, zDirection);
 
         RaycastHit hit;
-        int layer_mask = LayerMask.GetMask("Bounce");
-        Debug.Log(layer_mask);
-        Ray ray = new Ray(transform.position, direction);
-        if (Physics.Raycast(ray, out hit,10f,layer_mask,QueryTriggerInteraction.Ignore))
+        Ray ray = new Ray(transform.position, _direction);
+        if (Physics.Raycast(ray, out hit,10f, _bounceLayerMask, QueryTriggerInteraction.Ignore))
         {
             Debug.Log("hit");
             Debug.Log(hit.collider.name);
-            _targetLocal = transform.InverseTransformPoint(hit.point);
+            _targetLocal = transform.parent.InverseTransformPoint(hit.point);
+            _lastTargetLocal = transform.parent.InverseTransformPoint(transform.position);
         }
         
-        move = true;
+        _move = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (move)
-        { 
+        if (_move)
+        {
             _step = speed * Time.deltaTime; 
-            transform.position = Vector3.MoveTowards(transform.position, transform.TransformPoint(_targetLocal), _step);
+            transform.position = Vector3.MoveTowards(transform.position, transform.parent.TransformPoint(_targetLocal), _step);
         }
         
     }
 
     public void Reset(Transform transform)
     {
-        move = false;
+        _move = false;
         this.transform.SetPositionAndRotation(transform.position, transform.rotation);
     }
 
     public void Pause()
     {
-        move = false;
+        _move = false;
     }
     
     public void Continue()
     {
-        move = true;
+        _move = true;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.TransformPoint(_targetLocal),0.01f);
+        Gizmos.DrawSphere(transform.parent.TransformPoint(_targetLocal),0.01f);
         Gizmos.color = Color.white;
-        Gizmos.DrawLine(transform.position, transform.TransformPoint(_targetLocal));
+        Gizmos.DrawLine(transform.position, transform.parent.TransformPoint(_targetLocal));
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.parent.TransformPoint(_lastTargetLocal),0.01f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -94,18 +98,18 @@ public class GameBall : MonoBehaviour
             Debug.Log("Trigger");
             Debug.Log(other.name);
             RaycastHit hit;
-            Ray ray = new Ray(transform.position, direction);
+            Ray ray = new Ray(transform.parent.TransformPoint(_lastTargetLocal), _direction);
             if (Physics.Raycast(ray, out hit,10f, _bounceLayerMask ,QueryTriggerInteraction.Ignore))
             {
-                direction = Vector3.Reflect(direction, hit.normal);
-                direction = new Vector3(direction.x, 0f, direction.z);
+                _direction = Vector3.Reflect(_direction, hit.normal);
+                _direction = new Vector3(_direction.x, 0f, _direction.z);
                 CalculateNextTarget();
             }
         }
         
         if (other.CompareTag("ThrowableObstacle"))
         {
-            direction = new Vector3(-1 * direction.x, -1 * direction.y, -1 * direction.z);
+            _direction = new Vector3(-1 * _direction.x, -1 * _direction.y, -1 * _direction.z);
             CalculateNextTarget();
             OnThrowableObstacleHit.Invoke();
         }
@@ -114,12 +118,15 @@ public class GameBall : MonoBehaviour
     private void CalculateNextTarget()
     {
         RaycastHit hit;
-        Ray ray = new Ray(transform.position, direction);
+        Ray ray = new Ray(transform.position, _direction);
         if (Physics.Raycast(ray, out hit,10f, _bounceLayerMask ,QueryTriggerInteraction.Ignore))
         {
             Debug.Log("next: " + hit.collider.name);
-                    
-            _targetLocal = transform.InverseTransformPoint(hit.point);
+
+            _lastTargetLocal = transform.parent.InverseTransformPoint(transform.position);
+            _targetLocal = transform.parent.InverseTransformPoint(hit.point);
+            Debug.Log("_lastTargetLocal: " + _lastTargetLocal);
+            Debug.Log("_targetLocal: " + _targetLocal);
             //_target = new Vector3(hit.point.x, 0 , hit.point.z);
             //_target = (transform.position + direction) * 10f;
         }
